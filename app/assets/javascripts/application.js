69ecb34965f2c095aaaa8b22af7fb8ecbd492d20
gijs4 <gijs@de4gees.nl>
2016-07-07 09:46:06 +0000

    Asset path t.b.v. deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,36 @@
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 
+
+//= require maps/jquery-jvectormap-2.0.3.min.js
+//= require maps/gdp-data.js
+//= require maps/jquery-jvectormap-world-mill-en.js
+//= require maps/jquery-jvectormap-us-aea-en.js
+
+
+//= require bootstrap.min.js
+
+//= require gauge/gauge.min.js
+//= require gauge/gauge_demo.js
+
+//= require progressbar/bootstrap-progressbar.min.js
+
+//= require icheck/icheck.min.js
+
+//= require moment/moment.min.js
+//= require datepicker/daterangepicker.js
+
+//= require chartjs/chart.min.js
+
+//= require main.js
+//= require custom.js
+
+//= require pace/pace.min.js
+
+//= require select/select2.full.js
+
+//= require dropzone/dropzone.js
+
 //= require chartkick
 //= require toastr
 //= require_tree .

6ed5ec4060ae7a2263ab9e86ae0a7ef1f223d10b
gijs4 <gijs@de4gees.nl>
2016-07-08 10:27:27 +0000

    Change fontawesome files

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Workspace
     config.active_record.raise_in_transactional_callbacks = true
     
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'koenders', 'production')
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
   end
 end

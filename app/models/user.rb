f556ec32c4455331da2bcaa7a175b78d681c00ef
gijs4 <gijs@de4gees.nl>
2016-10-16 19:31:36 +0000

    Several devise improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
+
   belongs_to :company
   
   validates :company_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007101508) do
+ActiveRecord::Schema.define(version: 20161011201809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -198,6 +198,8 @@ ActiveRecord::Schema.define(version: 20161007101508) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "company_id"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.datetime "locked_at"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree

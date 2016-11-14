fdcd8ce8afa18c0358c4bf59d6b2759017344d2f
gijs4 <gijs@de4gees.nl>
2016-11-14 11:18:00 +0000

    Strip in client model and changes of form because of changing client_name to client_first_name and client_last_name

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,7 @@ class Client < ActiveRecord::Base
     belongs_to :company
     has_many :invoices
     has_many :projects
+    before_save :strip_blank_spaces
     after_create :define_client_number
         
     validates :client_first_name, presence: true, uniqueness: { scope: [:client_last_name] }
@@ -20,5 +21,14 @@ class Client < ActiveRecord::Base
     def define_client_number
         self.update_attributes(:client_number => self.id + ((self.client_number + 100) * 1000))
     end
+    
+    def strip_blank_spaces
+        self.client_first_name.strip
+        self.client_last_name.strip
+        self.client_email.strip
+        self.client_street_number.strip
         
+        self.client_phone.strip unless self.client_company_name.nil?
+        self.client_company_name.strip unless self.client_company_name.nil?
+    end        
 end
